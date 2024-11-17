abe1de282072bdf5321d23a1418121690f813332
Ceydaay <cydylmz.99@gmail.com>
2024-11-17 16:55:28 +0300

    Add project files.

---
+++ CodeFirstBasicApi/Data/PatikaFirstDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodeFirstBasicApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CodeFirstBasicApi.Data
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public PatikaFirstDbContext(DbContextOptions<PatikaFirstDbContext> options) : base(options)
+        {
+        }
+
+        DbSet<GameEntity> Games => Set<GameEntity>();
+        DbSet<MovieEntity> Movies => Set<MovieEntity>();
+    }
+}
